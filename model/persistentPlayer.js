9980f08c5ecb218ba36d9f63161ff60e64338fab
feng <feng@battlefy.com>
2015-07-02 13:19:19 -0700

    added update route

--- model/persistentPlayer.js
+++ model/persistentPlayer.js
@@ -4,17 +4,31 @@ var persistentPlayerSchema = require('../schema/persistentPlayer.json');
 var mockDB = {
   insert: function(entity, cb) {
     return cb(undefined, entity);
+  },
+  update: function(entity, cb) {
+    return cb(undefined, entity);
   }
 };
 
 var create = function(persistentPlayerEntity, cb) {
   var isValid = validator.validate(persistentPlayerEntity, persistentPlayerSchema);
+  console.log(isValid);
   if (!isValid) {
     return cb(new Error('Create Error!'))
   }
   return mockDB.insert(persistentPlayerEntity, cb);
 };
 
+var update = function(persistentPlayerEntity, cb) {
+  var isValid = validator.validate(persistentPlayerEntity, persistentPlayerSchema);
+  console.log(isValid);
+  if (!isValid) {
+    return cb(new Error('Update Error!'))
+  }
+  return mockDB.update(persistentPlayerEntity, cb);
+};
+
 module.exports = {
-  create: create
+  create: create,
+  update: update
 };
--- test/model/persistentPlayer.js
+++ test/model/persistentPlayer.js
@@ -16,14 +16,52 @@ var badPP = {
 describe('Game model', function() {
 
   it('should create', function() {
-    PersistentPlayer.create(goodPP, function(err, data){
+    PersistentPlayer.create(goodPP, function(err, data) {
       should.not.exist(err);
       data.should.eql(goodPP);
     });
   });
 
   it('should not create', function() {
-    PersistentPlayer.create(badPP, function(err, data){
+    PersistentPlayer.create(badPP, function(err, data) {
+      should.exist(err);
+      should.not.exist(data);
+    });
+  });
+
+  it('should update', function() {
+    var updatePP = {
+      _id: new ObjectID(),
+      email: 'updated email'
+    };
+    PersistentPlayer.update(updatePP, function(err, data) {
+      should.not.exist(err);
+      data.should.eql(updatePP);
+    });
+  });
+
+  it('should update again', function() {
+    var updatePP = {
+      _id: new ObjectID(),
+      inGameName: 'new name'
+    };
+    PersistentPlayer.update(updatePP, function(err, data) {
+      should.not.exist(err);
+      data.should.eql(updatePP);
+    });
+  });
+
+  it('should not update', function() {
+    PersistentPlayer.update({
+      inGameName: 'new name'
+    }, function(err, data) {
+      should.exist(err);
+      should.not.exist(data);
+    });
+  });
+
+  it('should not update again', function() {
+    PersistentPlayer.update({ _id: 'lollololo', inGameName: 'new name' }, function(err, data) {
       should.exist(err);
       should.not.exist(data);
     });
